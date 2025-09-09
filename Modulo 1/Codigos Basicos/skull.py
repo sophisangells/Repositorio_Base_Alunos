d1855d8753ffa6affff819f18cdf76492d8e612b
sophis <sophiacostapittner@gmail.com>
2025-09-09 13:43:22 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/skull.py
@@ -0,0 +1,9 @@
+nome=input("qual é o seu nome?")
+idade=input("qual sua idade?")
+
+print("---------------")
+print("nome da pessoa:",nome)
+print("idade da pessoa:",idade)
+print("-----------")
+print(f"nome da pessoa:{nome}")
+print(f"idade da pessoa:{idade}")
